b031a49593c45baa8d31a47ee5d3d9f0f86d75c5
greenblat17 <leftemail17@mail.ru>
2022-08-22 15:44:02 +0300

    gradle

---
+++ planner-gateway/src/test/java/com/greenblat/planner/plannergateway/PlannerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenblat.planner.plannergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
