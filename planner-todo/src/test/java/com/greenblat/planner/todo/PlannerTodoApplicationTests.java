39a847391d824ab61a643356f5f49c4acea1d73e
greenblat17 <leftemail17@mail.ru>
2022-08-22 16:35:13 +0300

    add tood module

---
+++ planner-todo/src/test/java/com/greenblat/planner/todo/PlannerTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenblat.planner.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
